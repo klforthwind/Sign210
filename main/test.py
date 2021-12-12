a97ffc1d0b75762319cea7cefaf0bf200f9ad60b
Forthwind <kl.forthwind@gmail.com>
2021-12-11 18:54:26 -0600

    finalize test.py for now

--- main/test.py
+++ main/test.py
@@ -1,29 +1,41 @@
 # pip3 install python-dotenv
 from dotenv import load_dotenv
 from os.path import exists
+from os.path import getsize
 import os
 
-import board
-import neopixel
-from PIL import Image
-from adafruit_pixel_framebuf import PixelFramebuffer
-
+if not exists(".env"):
+    raise Exception(".env file not found")
 
 while True:
-    
-    # make sure .env exists
-    if not exists(".env"):
-        os.system(f"curl https://www.klforthwind.com/sign210/.env > .env")
     load_dotenv()
 
-    NEW_PYTHON = os.getenv('PROJECT_UPDATES')
-    try:
-        os.system(f"curl {NEW_PYTHON} > new.py")
-        x = os.popen('diff curr.py new.py').read()
-        if len(x) != 0:
-            os.system(f"cp new.py curr.py")
+    # filename of current python file being ran by this program
+    CURR_PY = os.getenv('CURR_PY')
 
-        os.system("python3 new.py")
-        # os.system("sudo python3 new.py")
+    # filename of latest python downloaded from url
+    LATEST_PY = os.getenv('LATEST_PY')
+
+    # url of latest python to download
+    PY_FILE_LOC = os.getenv('PROJECT_UPDATES')
+    
+    try:
+        # curl latest python from server
+        os.system(f"curl {PY_FILE_LOC} > {LATEST_PY}")
+
+        # verify curr_py exists (in order to diff it)
+        if not exists(CURR_PY):
+            os.system(f"touch {CURR_PY}")
+
+        # obtain changes between latest python and current python
+        changes = os.popen(f"diff {CURR_PY} {LATEST_PY}").read()
+        
+        # update on change and latest_py isn't 404 (178bytes)
+        if len(changes) != 0 and getsize(LATEST_PY) > 210:
+            os.system(f"cp {LATEST_PY} {CURR_PY}")
+
+        # run python files using sudo - neopixel lights require sudo
+        os.system(f"sudo python3 {LATEST_PY}")
     except:
+        # expecting errors on new updates - don't want program to crash
         pass
