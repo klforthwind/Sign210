cf2ad1c171f32c07762632850e4999b1f7d1f780
Forthwind <kl.forthwind@gmail.com>
2021-12-12 03:04:58 -0600

    add some scripts that exist right now

--- scripts/P_QUEUE.sql
+++ scripts/P_QUEUE.sql
@@ -1,6 +1,7 @@
-CREATE TABLE EVENTS (
+CREATE TABLE P_QUEUE (
     id int NOT NULL AUTO_INCREMENT,
     ev_type varchar(128) NOT NULL,
     ev_extra varchar(512) NOT NULL,
+    importance int NOT NULL,
     PRIMARY KEY (id)
 );
