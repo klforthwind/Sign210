1dc70838b6a50e1cf450ee63fa5d9c20fb13c53b
Forthwind <kl.forthwind@gmail.com>
2021-12-12 21:04:52 -0600

    update sql scripts

--- scripts/COMMANDS.sql
+++ scripts/COMMANDS.sql
@@ -1,5 +1,5 @@
 CREATE TABLE COMMANDS (
     command varchar(64) NOT NULL,
-    pic_loc varchar(128) NOT NULL,
+    pic_name varchar(128) NOT NULL,
     PRIMARY KEY (command)
 );
--- scripts/PICTURES.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE PICTURES (
-    command varchar(64) NOT NULL,
-    pic_name varchar(128) NOT NULL,
-    PRIMARY KEY (command)
-);
