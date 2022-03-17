bf42fa8432fe7b9d41e8d6097ab60bb4f18c3779
Forthwind <kl.forthwind@gmail.com>
2022-03-16 21:38:44 -0500

    table_handler optimizations

--- main/table_handler.py
+++ main/table_handler.py
@@ -1,8 +1,10 @@
+from collections import defaultdict
 from extra_funcs import *
 import json
 
 class TableHandler():
-    ev_priority = {
+    ev_priority = defaultdict(
+        lambda:0, {
         "ALLCLEAR": 13,
         "RAID": 12,
         "CHEER": 10,
@@ -18,62 +20,51 @@ class TableHandler():
         "JOINREALM": 2,
         "CUSTOMREWARD": 2,
         "NORMALCHAT": 0
-    }
+    })
 
     def get_event(self, db):
-        # [(id, ev_type, ev_cmd, ev_msg, ev_extra, importance)]
-        res = db.query("SELECT * FROM P_QUEUE ORDER BY importance DESC")
+        """Get event from P_QUEUE table."""
+        res = db.query("SELECT id, ev_type, ev_cmd, ev_msg, ev_extra, importance FROM P_QUEUE ORDER BY importance DESC")
 
         if len(res) == 0:
             return
         
-        ev = list(res[0])
-        if ev[1] != "GAMECHANGE" and ev[1] != "FOLLOW":
-            try:
-                ev[4] = json.loads(ev[4])
-            except:
-                pass
+        event = list(res[0])
+        event[4] = json.loads(event[4])
 
-        if self.should_clear(ev):
+        if self.should_clear(event):
             db.execute(f"DELETE FROM P_QUEUE WHERE 1=1")
             return
 
-        db.execute(f"DELETE FROM P_QUEUE WHERE id={ev[0]}")
-
-        return ev
+        db.execute(f"DELETE FROM P_QUEUE WHERE id={event[0]}")
+        return event
 
     def process_events(self, db):
-        # [(id, ev_type, ev_cmd, ev_msg, ev_extra)]
-        res = db.query("SELECT * FROM EVENTS")
+        """Process events from EVENTS table into P_QUEUE table."""
+        res = db.query("SELECT id, ev_type, ev_cmd, ev_msg, ev_extra FROM EVENTS")
 
-        res_count = len(res)
-        if res_count == 0:
+        if len(res) == 0:
             return
+        
+        latest_id = res[len(res)-1][0]
 
-        latest_id = res[res_count-1][0]
+        for row in res:
+            ev_type = row[1]
+            priority = self.ev_priority[ev_type]
 
-        # [(id, ev_type, ev_cmd, ev_msg, ev_extra)]
-        for x in res:
-            if not valid_msg(x[2]) or not valid_msg(x[3]):
-                continue
-            
-            ev_type = x[1]
-            if ev_type in self.ev_priority:
-                priority = self.ev_priority[ev_type]
-
-                if ev_type == "COMMAND":
-                    cmd = x[2].upper()
-                    if cmd == "CLEAR" or cmd == "ALLCLEAR":
-                        priority = self.ev_priority[cmd]
-                sql = "INSERT INTO P_QUEUE (ev_type, ev_cmd, ev_msg, ev_extra, importance) " + \
-                    f"VALUES ('{ev_type}', '{x[2]}', '{x[3]}', '{x[4]}', {priority})"
-                db.execute(sql)
+            if ev_type == "COMMAND":
+                cmd = row[2].upper()
+                if cmd == "CLEAR" or cm == "ALLCLEAR":
+                    priority = self.ev_priority[cmd]
+            sql = "INSERT INTO P_QUEUE (ev_type, ev_cmd, ev_msg, ev_extra, importance) " + \
+                f"VALUES ('{ev_type}', '{row[2]}', '{row[3]}', '{row[4]}', {priority})"
+            db.execute(sql)
 
         db.execute(f"DELETE FROM EVENTS WHERE id <= {latest_id}")
 
     # event = (id, ev_type, ev_extra, importance)
     def should_clear(self, event):
-        return (event[1] == "COMMAND" and 
-            (event[2].upper() == "ALLCLEAR" or
-            event[2].upper() == "CLEAR") and
+        """Returns whether the P_QUEUE table should be cleared based on current event."""
+        return (event[1] == "COMMAND" and
+            event[2].upper() in ["ALLCLEAR","CLEAR"] and 
             (event[4]['flags']['mod'] or event[4]['flags']['broadcaster']))
