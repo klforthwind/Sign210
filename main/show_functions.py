b80d44bf76765f3e1943725e5c0da811ecfaac14
Forthwind <kl.forthwind@gmail.com>
2022-03-16 23:34:00 -0500

    major clean up

--- main/show_functions.py
+++ main/show_functions.py
@@ -5,73 +5,20 @@ from extra_funcs import *
 import numpy as np
 import time
 
-#
-#
-#
-#
 def rick(event, db, pixels):
+    """Loops Rick on the matrix and colors on the strip for 10 seconds"""
+    DURATION = 10
     db.set_evar(db.CURR_MAT, "")
     j = 0
-    end_time = time.time() + 10
+    end_time = time.time() + DURATION
     while time.time() < end_time:
         color = wheel(j & 255)
         pixels.show(f"rick/{(j//8)%21}.png", [color]*124)
         time.sleep(0.0125)
         j+=2
 
-#
-#
-#
-#
-# def snake(event, db, pixels):
-#     strip_len = pixels.STRIP_LEN
-#     color = (255,0,0)
-#     if event[4]['user'].lower() == "beta64":
-#         color = (255,0,255)
-#     start = randint(0,strip_len-1)
-#     size = 5
-
-#     apple = randint(0,strip_len-1)
-
-#     # while size < strip_len:
-#     while size < 10:
-#         old_start = start
-#         start = (start + 5) % strip_len
-#         if start >= apple >= old_start:
-#             size += 1
-#             if start + size >= strip_len:
-#                 apple = randint((start + size) % strip_len, start-1)
-#             else:
-#                 if start != 0:
-#                     apple = (randint(0, start-1), randint(start+size,strip_len-1))[randint(0,1)]
-#                 else:
-#                     apple = randint(start+size,strip_len-1)
-        
-#         strip = []
-#         for x in range(124):
-#             if x == apple:
-#                 strip.append(color)
-#             elif start + size >= strip_len:
-#                 if x <= (start + size) % strip_len:
-#                     strip.append(color)
-#                 elif x >= start:
-#                     strip.append(color)
-#                 else:
-#                     strip.append((0,0,0))
-#             else:
-#                 if start <= x < start+size:
-#                     strip.append(color)
-#                 else:
-#                     strip.append((0,0,0))
-        
-#         pixels.color_strip(strip)
-#         time.sleep(0.01)
-
-#
-#
-#
-#
 def loading_raid(event, db, pixels):
+    """Loads hat with color, and then shows raid.png at end."""
     db.set_evar(db.CURR_MAT, "raid.png")
     strip_len = pixels.STRIP_LEN
     strip = get_strip(db)
@@ -108,70 +55,52 @@ def loading_raid(event, db, pixels):
     pixels.show("raid.png", strip)
     time.sleep(2)
 
-#
-#
-#
-#
 def animate_raid(event, db, pixels):
+    """Runs loading_raid and then runs Rick if triggered by broadcaster or mod."""
     loading_raid(event, db, pixels)
     if event[4]['flags']['vip'] or event[4]['flags']['broadcaster']:
         rick(event, db, pixels)
 
-    set_default(db, pixels)
-
-#
-#
-#
-#
 def pulse(db, pixels, image, rgb1_list, rgb2_list, run_time):
+    """Shows image and pulse of colors using lerp function over time."""
     db.set_evar(db.CURR_MAT, "")
     low = np.array(rgb1_list)
     high = np.array(rgb2_list)
     lerp_colors(pixels, low, high, image, run_time)
 
-#
-#
-#
-#
 def slow_rainbow(event, db, pixels):
+    """Shows rotating mario star and strip changes rainbow colors."""
+    DURATION = 10
     db.set_evar(db.CURR_MAT, "")
     j = 0
-    end_time = time.time() + 10
+    end_time = time.time() + DURATION
     while time.time() < end_time:
         color = wheel(j & 255)
         pixels.show(f"star/star{(j//4)%5+1}.png", [color]*124)
         time.sleep(0.0125)
         j+=2
 
-    set_default(db, pixels)
-
-#
-#
-#
-#
 def fast_rainbow(event, db, pixels):
+    """Shows rotating mario star and strip changes rainbow colors fast."""
+    DURATION = 10
     db.set_evar(db.CURR_MAT, "")
     j = 0
     bri = 64    # brightness
     colors = [(0,0,bri),(bri,0,bri),(bri,0,0),(bri,bri,0),(0,bri,0),(bri,0,0),(bri,bri,bri)]
 
-    end_time = time.time() + 10
+    end_time = time.time() + DURATION
     while time.time() < end_time:
         color = colors[(j%7)]
         pixels.show(f"star/star{(j//2)%5+1}.png", [color]*124)
         time.sleep(0.025)
         j+=1
 
-    set_default(db, pixels)
-
-#
-#
-#
-#
 def slow_rainbow_shift(event, db, pixels):
+    """Shows rotating mario star and strip shifts (clockwise?) rainbow colors."""
+    DURATION = 10
     db.set_evar(db.CURR_MAT, "")
     j = 0
-    end_time = time.time() + 10
+    end_time = time.time() + DURATION
     while time.time() < end_time:
         strip = []
         for x in range(124):
@@ -181,13 +110,9 @@ def slow_rainbow_shift(event, db, pixels):
         time.sleep(0.025)
         j+=1
 
-    set_default(db, pixels)
-
-#
-#
-#
-#
 def show_game(db, pixels, game_title, wait_time):
+    """Changes matrix to game_title for a certain amount of time."""
+
     db.set_evar(db.CURR_MAT, "")
     strip = get_strip(db)
 
@@ -207,10 +132,9 @@ def show_game(db, pixels, game_title, wait_time):
             pixels.show(games[game], strip)
             time.sleep(wait_time)
             break
-    
-    set_default(db, pixels)
 
 def cheer(event, db, pixels):
+    """Shows bits picture on matrix and shifts purple, blue, and green around (clockwise?) the hat."""
     colors = [(153,51,255),(0,0,255),(0,255,0)]
     offset = 0
 
@@ -226,17 +150,9 @@ def cheer(event, db, pixels):
         pixels.show("bits.png", arr)
 
         time.sleep(0.1)
-    
-    set_default(db, pixels)
 
-
-
-
-#
-#
-#
-#
 def lerp_colors(pixels, rgb1, rgb2, pic_name, run_time):
+    """Shows picture pic_name and lerps colors based on min / max rgb1 and rgb2 values using run_time."""
     diff = rgb2 - rgb1
     change = diff
     j = 0
@@ -257,13 +173,8 @@ def lerp_colors(pixels, rgb1, rgb2, pic_name, run_time):
         j += 1
         time.sleep(0.025)
 
-#
-#
-#
-#
 def wheel(pos):
-    # Input a value 0 to 255 to get a color value.
-    # The colours are a transition r - g - b - back to r.
+    """Generates and returns RGB value based on pos 0-255 (R -> G -> B -> R)."""
     if pos < 0 or pos > 255:
         r = g = b = 0
     elif pos < 85:
