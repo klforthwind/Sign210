fad0938e22ad6ed84ae42dcf186f7ebafcfadef6
Forthwind <kl.forthwind@gmail.com>
2022-03-17 18:39:02 -0500

    use unofficial git pull, due to bad marshall

--- main/run.py
+++ main/run.py
@@ -1,5 +1,5 @@
 from db_conn import *
-import git
+# import git
 import os
 
 db = DBConn()
@@ -10,8 +10,9 @@ db.reset_config()
 db.disconnect()
 
 while True:
-    g = git.Git('.')
-    g.pull('origin','development')
+    # g = git.Git('.')
+    # g.pull('origin','main')
+    os.system("git pull")
 
     # Run python files using sudo - neopixel lights require sudo
     os.system("sudo python3 main.py")
