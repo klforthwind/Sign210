456bce4a72b82d4963e1b31c254d8b2c7d7e5147
klforthwind <kl.forthwind@gmail.com>
2022-03-31 18:05:21 -0500

    update 484 hat

--- main/command.py
+++ main/command.py
@@ -90,7 +90,7 @@ def exec_command(event, db, pixels):
             "blakehat": ("blake.png", "0,0,200"),
             "rbkhat": ("rootbeerking.png", "0,240,0"),
             "mokihat": ("craft_inc.png", "100,33,0"),
-            "484hat": ("pac_ghost.png", "240,0,0"),
+            "484hat": ("pac_ghost.png", "255,255,0"),
             "morguehat": ("skull.png", "0,181,207")
         }
 
